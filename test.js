68e312a488a7cb3bffed474a9475fbb292ac1c4e
Seun Agbeye <agbeyeseun1@gmail.com>
2017-10-24 16:27:58 +0100

    Updated the wrong description on the test.js

--- test.js
+++ test.js
@@ -10,25 +10,25 @@ describe('rainDrops', () => {
 	});
 
 	describe('Invalid Input', () => {
-		it('It Should return ', () => {
+		it('It Should return "Not a whole number" ', () => {
 			assert.equal(rainDrops(1755.22), "Not a whole number");
 		});
 	});
 
 	describe('Invalid Input', () => {
-		it('It Should return "Number must be greater than 0" ', () => {
+		it('It Should return "Number must be greater than 0" for 0', () => {
 			assert.equal(rainDrops(0), "Number must be greater than 0");
 		});
 	});
 
 	describe('Invalid Input', () => {
-		it('It Should return ', () => {
+		it('It Should return "Number must be greater than 1" for 1', () => {
 			assert.equal(rainDrops(1), "Number must be greater than 1");
 		});
 	});
 
 	describe('Invalid Input', () => {
-		it('It Should return "Number must ber greater than 0"', () => {
+		it('It Should return "Number must ber greater than 0" for -1175', () => {
 			assert.equal(rainDrops(-1175), "Number must be greater than 0");
 		});
 	});
@@ -50,19 +50,19 @@ describe('rainDrops', () => {
 		});
 	});
 
-	describe('Invalid Input', () => {
+	describe('Valid Input', () => {
 		it('It Should return "Plong" for 28', () => {
 			assert.equal(rainDrops(28), "Plong");
 		});
 	});
 
-	describe('Invalid Input', () => {
+	describe('Valid Input', () => {
 		it('It Should return "PlingPlang" for 1755', () => {
 			assert.equal(rainDrops(1755), "PlingPlang");
 		});
 	});
 
-	describe('Invalid Input', () => {
+	describe('Valid Input', () => {
 		it('It Should return "Plang" for 25', () => {
 			assert.equal(rainDrops(25), "Plang");
 		});
